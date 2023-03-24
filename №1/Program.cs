d1d2dae1787b39d17464f72b81d70d4ac34d1a7e
Nadezhda Chumachenko <chnadezhda22@gmail.com>
2023-03-24 17:13:02 +0200

    Added first exercise

---
+++ №1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдает, какое число большее, а какое меньшее
+
+int a = 0;
+int b = 0;
+
+Console.WriteLine("Введите два числа: ");
+
+
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+{
+    if (a > b)
+    {
+        Console.WriteLine("Наибольшее число: " + a);
+        Console.WriteLine("Наименьшее число: " + b);
+    }
+
+    else
+    {
+        Console.WriteLine("Наибольшее число: " + b);
+        Console.WriteLine("Наименьшее число: " + a);
+        
+    }
+}
